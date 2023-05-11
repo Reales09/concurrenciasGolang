1069106651dcd5ba2ea0a75d3b87389c9d2abd1f
Reales Myles <rclarence123@gmail.com>
2023-05-11 12:53:30 -0500

    Channels - canales

--- servidores/servidores.go
+++ servidores/servidores.go
@@ -6,28 +6,36 @@ import (
 	"time"
 )
 
-func revisarServidores(servidor string) {
+func revisarServidores(servidor string, canal chan string) {
 	_, error := http.Get(servidor)
 
 	if error != nil {
-		fmt.Println(servidor, "no esta disponible")
+		// fmt.Println(servidor, "no esta disponible")
+		canal <- servidor + " no esta disponible"
 	} else {
-		fmt.Println(servidor, "esta funcionando correctamente")
+		// fmt.Println(servidor, "esta funcionando correctamente")
+		canal <- servidor + " esta funcionando"
 	}
 }
 
 func main() {
 	inicio := time.Now()
+	canal := make(chan string)
+
 	servidores := []string{
 		"http://platzi.com",
 		"http://google.com",
 		"http://facebook.com",
 		"http://instagram.com",
+		"https://gpsmobile.net/",
 	}
 
 	for _, servidor := range servidores {
-		go revisarServidores(servidor)
+		go revisarServidores(servidor, canal)
 		// revisarServidores(servidor)
+	}
+	for i := 0; i < len(servidores); i++ {
+		fmt.Println(<-canal)
 	}
 	tiempoPaso := time.Since(inicio)
 	fmt.Printf("Tiempo de ejecucion %s\n", tiempoPaso)
