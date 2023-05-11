2552afd1be108c9556f8cae809cd741a66002e62
Reales Myles <rclarence123@gmail.com>
2023-05-11 12:37:44 -0500

    Primer commit concurrencias

---
+++ servidores/servidores.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func revisarServidores(servidor string) {
+	_, error := http.Get(servidor)
+
+	if error != nil {
+		fmt.Println(servidor, "no esta disponible")
+	} else {
+		fmt.Println(servidor, "esta funcionando correctamente")
+	}
+}
+
+func main() {
+	inicio := time.Now()
+	servidores := []string{
+		"http://platzi.com",
+		"http://google.com",
+		"http://facebook.com",
+		"http://instagram.com",
+	}
+
+	for _, servidor := range servidores {
+		revisarServidores(servidor)
+	}
+	tiempoPaso := time.Since(inicio)
+	fmt.Printf("Tiempo de ejecucion %s\n", tiempoPaso)
+}
